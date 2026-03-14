25c0eec971b9531cfcca33c7c6825674cae7fe1a
Rutuja shelar <122372944+rutujashelar2601@users.noreply.github.com>
2026-03-14 19:06:13 +0530

    Create Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,13 @@
+public class Pattern4 {
+	public static void main(String[] args) {
+		int n = 4;
+		int size = 2 * n - 1;
+		for (int i = 0; i < 2 * n - 1; i++) {
+			for (int j = 0; j < 2 * n - 1; j++) {
+				int value = n - Math.min(Math.min(i, j), Math.min(size - 1 - i, size - 1 - j));
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}
+	}
+}
